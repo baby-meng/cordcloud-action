aac75db2bf0343c8c2bae9f1679e765cb1a32a35
baby-meng <56985958+baby-meng@users.noreply.github.com>
2025-06-15 18:51:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+
+
 from string import Template
 
 import pyotp
@@ -5,7 +8,7 @@ from actions_toolkit import core
 
 from app import log
 from app.action import Action
-
+os.environ["DEBUG"] = "true"  # 启用详细调试
 action = {
     'action': 'CordCloud Action',
     'author': 'Yang Libin',
@@ -15,7 +18,15 @@ action = {
 
 welcome = Template('欢迎使用 $action ❤\n\n📕 入门指南: $marketplace\n📣 由 $author 维护: $github\n')
 log.info(welcome.substitute(action))
-
+if os.environ.get("DEBUG") == "true":
+    import http.client
+    http.client.HTTPConnection.debuglevel = 1
+    import logging
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
 try:
     # 获取输入
     email = core.get_input('email', required=True)
